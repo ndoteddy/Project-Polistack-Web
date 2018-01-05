1fa7303d6b23fa7a41d78ad491b564c706e0e0b5
ndoteddy <nando.teddy@gmail.com>
2018-01-05 16:20:14 +0700

    fix backbone header controller )CROS)

--- server.js
+++ server.js
@@ -33,22 +33,24 @@ routes(app); //register the route
 
 app.listen(port);
 
-app.use(function(req, res) {
+app.use(function(req, res, next) {
     // var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:8080', 'http://localhost:8080'];
     // var origin = req.headers.origin;
     // if(allowedOrigins.indexOf(origin) > -1){
     //     res.setHeader('Access-Control-Allow-Origin', origin);
     // }
     // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.header("Access-Control-Allow-Origin", "*");
+
     // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.header('Access-Control-Allow-Credentials', true);
 
+
+
+    next();
 
 });
 
